05fabc9c0db448208a519c6bd74a8b47a9bbe234
Vincenzo De Pascale <vincenzodepascala0@gmail.com>
2023-02-27 11:06:43 +0100

    add files

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 //import logo from "./logo.svg";
-import { BrowserRouter, Routes } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 
 function App() {
