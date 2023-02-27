4938518bdff5a205debc715066d07b4592b04440
Vincenzo De Pascale <vincenzodepascala0@gmail.com>
2023-02-27 16:25:56 +0100

    add env

--- src/Components/Aside.jsx
+++ src/Components/Aside.jsx
@@ -3,15 +3,15 @@ import { Row, Col, Button, Image } from "react-bootstrap";
 import { AiFillQuestionCircle } from "react-icons/ai";
 import { BsThreeDots } from "react-icons/bs";
 import TokenUtenti from "./TokenUtenti";
-import Logo from "../assets/logoEpicode.jpg"
+import Logo from "../assets/logoEpicode.jpg";
 
 const Aside = () => {
   const [utenti, setUtenti] = useState();
 
   const AsideFetch = async () => {
     try {
-      const token =
-        "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2M2ZjNjY0OGYxOTNlNjAwMTM4MDdmNGYiLCJpYXQiOjE2Nzc0ODU3MTIsImV4cCI6MTY3ODY5NTMxMn0.K-x1r1f3GI44gbmbavOGWzuo0OEpPf5qkw5L1mJaNLI";
+      const token = process.env.REACT_APP_TOKEN;
+
       const response = await fetch(
         "https://striveschool-api.herokuapp.com/api/profile/",
         {
@@ -38,7 +38,14 @@ const Aside = () => {
   return (
     <div className="m-3">
       {/* primo div */}
-      <div className="mb-2 p-2 text-secondary rounded" style={{ fontSize: "0.95em", fontWeight: "500" , border: "1px solid lightgray" }}>
+      <div
+        className="mb-2 p-2 text-secondary rounded"
+        style={{
+          fontSize: "0.95em",
+          fontWeight: "500",
+          border: "1px solid lightgray",
+        }}
+      >
         <div className=" linkAside">
           Modifica il tuo profilo e l'URL
           <span className="ps-2">
@@ -54,34 +61,61 @@ const Aside = () => {
         </div>
       </div>
       {/* secondo div */}
-      <div className="mb-2 p-2 text-secondary text-center rounded" style={{fontSize: "0.9em", fontWeight: "500", border: "1px solid lightgray" }}>
-        <div className="d-flex justify-content-end text-dark"  style={{fontSize: "0.7em"}}>
-          <p>Annuncio 
-          <span className="ms-1">
-            <BsThreeDots />
-          </span></p>
+      <div
+        className="mb-2 p-2 text-secondary text-center rounded"
+        style={{
+          fontSize: "0.9em",
+          fontWeight: "500",
+          border: "1px solid lightgray",
+        }}
+      >
+        <div
+          className="d-flex justify-content-end text-dark"
+          style={{ fontSize: "0.7em" }}
+        >
+          <p>
+            Annuncio
+            <span className="ms-1">
+              <BsThreeDots />
+            </span>
+          </p>
         </div>
         <div>L'azienda che ti aiuta a costruire il tuo futuro</div>
         <div>
-          <Image className="rounded-circle"
-            style={{ width: "40%" }} src={Logo}/>
+          <Image
+            className="rounded-circle"
+            style={{ width: "40%" }}
+            src={Logo}
+          />
         </div>
         <div>EPICODE</div>
         <div className="d-flex justify-content-center mt-2">
-          <Button href="https://www.epicode.com" target="_blank">Segui</Button>
+          <Button href="https://www.epicode.com" target="_blank">
+            Segui
+          </Button>
         </div>
       </div>
       {/* terzo div */}
-      <div className="mb-2 p-2 rounded" style={{ border: "1px solid lightgray" }}>
-        <div style={{fontWeight:"600", fontSize: "1.15em"}} className="mb-1">Altre aziende consultate</div>
+      <div
+        className="mb-2 p-2 rounded"
+        style={{ border: "1px solid lightgray" }}
+      >
+        <div style={{ fontWeight: "600", fontSize: "1.15em" }} className="mb-1">
+          Altre aziende consultate
+        </div>
         {utenti &&
           utenti
             .filter((_, i) => i < 5)
             .map((e) => <TokenUtenti profile={e} />)}
       </div>
       {/* quarto div */}
-      <div className="mb-2 p-2 rounded" style={{ border: "1px solid lightgray" }}>
-        <div style={{fontWeight:"600", fontSize: "1.15em"}}  className="mb-1">Persone che potresti conoscere</div>
+      <div
+        className="mb-2 p-2 rounded"
+        style={{ border: "1px solid lightgray" }}
+      >
+        <div style={{ fontWeight: "600", fontSize: "1.15em" }} className="mb-1">
+          Persone che potresti conoscere
+        </div>
         {utenti &&
           utenti
             .filter((_, i) => i < 5)
