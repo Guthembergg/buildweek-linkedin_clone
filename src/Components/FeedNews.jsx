b2e54de5748dfe088b6ac0075ee0528af938d4a9
RobertoTursi <Robertotursi00@gmail.com>
2023-03-01 17:46:26 +0100

    d

---
+++ src/Components/FeedNews.jsx
@@ -0,0 +1,103 @@
+import { Card, Image, Form, NavDropdown } from "react-bootstrap";
+import { SlLike } from "react-icons/sl";
+import { AiOutlineComment } from "react-icons/ai";
+import { FiSend } from "react-icons/fi";
+import { ImLoop } from "react-icons/im";
+import { Link } from "react-router-dom";
+import { RiDeleteBin6Fill } from "react-icons/ri";
+import { HiOutlinePencil, HiDotsHorizontal } from "react-icons/hi";
+import { useNavigate } from "react-router-dom";
+import PostPencilModal from "./PostPencilModal";
+import PostDeleteModal from "./PostDeleteModal";
+
+import { useSelector } from "react-redux";
+const FeedNews = (props) => {
+  const myId = useSelector((state) => state.myProfile._id);
+  console.log(myId);
+  console.log(props);
+  console.log(props.news.user._id);
+  return (
+    <Card className="mb-3 p-2">
+      <section className="d-flex p-2">
+        <div>
+          <Image
+            roundedCircle={true}
+            style={{ width: "50px", height: "50px" }}
+            src={
+              props.news?.image
+                ? props.news.image
+                : "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png"
+            }
+          />
+        </div>
+        <div className="p-2">
+          <Link
+            className="text-decoration-none text-dark"
+            to={`/profile/${props.news?.user._id}`}
+          >
+            <h6 className="m-0">
+              {props.news?.user.name} {props.news?.user.surname}
+            </h6>
+          </Link>
+          <p className="m-0 text-secondary">{props.news?.user.title}</p>
+          <p className="m-0 text-secondary">
+            <span>
+              {props.news?.createdAt.slice(12, 16)} -{" "}
+              {props.news?.createdAt.slice(8, 10)}/
+              {props.news?.createdAt.slice(5, 7)}/
+              {props.news?.createdAt.slice(0, 4)}
+            </span>
+          </p>
+        </div>
+        <div>
+          {myId === props.news.user._id && (
+            <div className="d-flex ">
+              <NavDropdown
+                title={
+                  <span>
+                    <HiDotsHorizontal />
+                  </span>
+                }
+                id="collasible-nav-dropdown"
+              >
+                <PostPencilModal id={props.news._id} text={props.news.text} />
+                <PostDeleteModal id={props.news._id} />
+              </NavDropdown>
+            </div>
+          )}
+        </div>
+      </section>
+      <Card.Body className="border-top">
+        <p>{props.news.text}</p>
+      </Card.Body>
+      <section className="d-flex justify-content-around text-tertiary border-top">
+        <div className="iconPost rounded">
+          <span className="me-2">
+            <SlLike />
+          </span>{" "}
+          <span className="d-none d-md-inline">Consiglia </span>
+        </div>
+        <div className="iconPost rounded">
+          <span className="me-2">
+            <AiOutlineComment />
+          </span>{" "}
+          <span className="d-none d-md-inline">Commenta</span>{" "}
+        </div>
+        <div className="iconPost rounded">
+          <span className="me-2">
+            <ImLoop />
+          </span>{" "}
+          <span className="d-none d-md-inline">Diffondi il post</span>{" "}
+        </div>
+        <div className="iconPost rounded">
+          <span className="me-2">
+            <FiSend />
+          </span>{" "}
+          <span className="d-none d-md-inline">Invia</span>
+        </div>
+      </section>
+    </Card>
+  );
+};
+
+export default FeedNews;
---
+++ src/Components/ModalPost.jsx
@@ -0,0 +1,130 @@
+import { useState } from "react";
+import { Row, Col, Image, Form, Modal, Button } from "react-bootstrap";
+import Card from "react-bootstrap/Card";
+import {
+  BsFillCalendar2EventFill,
+  BsFillPlayBtnFill,
+  BsImageFill,
+} from "react-icons/bs";
+import { RiArticleFill } from "react-icons/ri";
+
+function NewPostProva() {
+  const [innerData, setInnerData] = useState({
+    text: "",
+  });
+
+  const token = process.env.REACT_APP_TOKEN;
+
+  const handleChange = (property, value) => {
+    setInnerData({ [property]: value });
+  };
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  const fetchNewsPost = async () => {
+    try {
+      const response = await fetch(
+        `https://striveschool-api.herokuapp.com/api/posts/`,
+        {
+          method: "POST",
+          headers: {
+            Authorization: `Bearer ${token}`,
+            Accept: "application/json",
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(innerData),
+        }
+      );
+      if (response.ok) {
+        /* const data = await response.json(); */
+        console.log("News: fetch Post. if ok");
+      } else {
+        console.log("News: fetch Post. errore in if");
+      }
+    } catch (err) {
+      console.log("News: fetch Post. err in catch");
+    }
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    fetchNewsPost();
+  };
+
+  return (
+    <>
+      <>
+        <Card body className="mb-3">
+          <Row>
+            <Col xs={2}>
+              <Image
+                roundedCircle={true}
+                style={{ width: "50px" }}
+                alt=""
+                src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png"
+              />
+            </Col>
+            <Col xs={10}>
+              <Form onClick={handleShow}>
+                <Form.Group className="ms-0 m-2 ">
+                  <Form.Control className="rounded-pill" type="text" placeholder="Avvia un post" />
+                </Form.Group>
+              </Form>
+            </Col>
+          </Row>
+          <Row className="d-flex justify-content-center align-items-center py-2">
+            <Col xs={3} md={2} className="d-flex justify-content-center align-items-center">
+              <BsImageFill  className="text-primary" /><span className="d-none d-md-block ms-2">Foto</span>
+            </Col>
+            <Col xs={3} md={2} className="d-flex justify-content-center align-items-center">
+              <BsFillPlayBtnFill  className="text-success" />  <span className="d-none d-md-block ms-2">Video</span>
+            </Col>
+            <Col xs={3} md={2} className="d-flex justify-content-center align-items-center">
+              <BsFillCalendar2EventFill style={{color: "brown"}} />  <span className="d-none d-md-block ms-2">Evento</span>
+            </Col>
+            <Col xs={3} md={4} className="d-flex justify-content-center align-items-center">
+              <RiArticleFill style={{color: "orange"}} />  <span className="d-none d-md-block ms-2">Scrivi un articolo</span>
+            </Col>
+          </Row>
+        </Card>
+      </>
+
+      <Modal show={show} onHide={handleClose} backdrop="static">
+        <Modal.Header closeButton>
+          <Modal.Title>Modifica informazioni</Modal.Title>
+        </Modal.Header>
+        <Modal.Body className="position-relative">
+          <Form onSubmit={handleSubmit}>
+            <Form.Group className="mb-3">
+              <Form.Label>Nuovo post</Form.Label>
+              <Form.Control
+                aria-selected
+                as="textarea"
+                placeholder="Inserisci qui il tuo post"
+                onChange={(e) => handleChange("text", e.target.value)}
+              />
+              <div className="d-flex justify-content-end">
+                <Form.Label>{innerData.text.length}/2600</Form.Label>
+              </div>
+            </Form.Group>
+            <Modal.Footer>
+              <Button
+                variant="primary"
+                type="submit"
+                onClick={() => {
+                  handleClose();
+                }}
+              >
+                Pubblica
+              </Button>
+            </Modal.Footer>
+          </Form>
+        </Modal.Body>
+      </Modal>
+    </>
+  );
+}
+
+export default NewPostProva;
