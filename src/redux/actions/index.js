5f567b9ffc8b837d307b5c8a576b8d5211eeffeb
Karim Guettech <karimguettech@gmail.com>
2023-03-07 17:38:43 +0100

    jobs

--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -3,9 +3,6 @@ export const ADD_TO_FAV = "ADD_TO_FAV";
 export const addToFav = (element) => {
   return (dispatch, getState, i) => {
     const currentState = getState();
-    console.log(currentState);
-    if (currentState.favourites.findIndex((job) => job === element) === -1) {
-      dispatch({ type: ADD_TO_FAV, payload: element });
-    }
+    dispatch({ type: ADD_TO_FAV, payload: element });
   };
 };
