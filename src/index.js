c4ad40aadcf701dd3f274f5c389fd761ef3bcb02
Karim Guettech <karimguettech@gmail.com>
2023-03-07 10:31:01 +0100

    persist

--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { Provider } from "react-redux";
-import store from "./redux/store";
-
+import { store, persistor } from "./redux/store";
+import { PersistGate } from "redux-persist/integration/react";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Provider store={store}>
-    <App />
+    <PersistGate persistor={persistor}>
+      <App />
+    </PersistGate>
   </Provider>
 );
--- src/redux/store/index.js
+++ src/redux/store/index.js
@@ -1,8 +1,17 @@
 import { configureStore } from "@reduxjs/toolkit";
 import MainReducer from "../reducers";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import { encryptTransform } from "redux-persist-transform-encrypt";
 
-const store = configureStore({
-  reducer: MainReducer,
+const persistConfig = {
+  key: "root",
+  storage: storage,
+  transform: [encryptTransform({ secretKey: process.env.REACT_APP_TOKEN })],
+};
+const persistedReducer = persistReducer(persistConfig, MainReducer);
+export const store = configureStore({
+  reducer: persistedReducer,
 });
 
-export default store;
+export const persistor = persistStore(store);
